31172cc362a98d83bfe8ae8d1666d398aea8a492
Kabiru Mwenja <makabby@gmail.com>
2026-04-01 23:23:46 +0300

    Add prettierd and erb_lint, remove eslint_d, use <leader>cf for format

--- nvim/.config/nvim/lua/plugins/none-ls.lua
+++ nvim/.config/nvim/lua/plugins/none-ls.lua
@@ -1,3 +1,10 @@
+-- None-ls (null-ls): Linting and formatting via external tools
+-- Diagnostics: rubocop (Ruby), erb_lint (ERB templates)
+-- Formatting: rubocop (Ruby), prettierd (JS/TS/HTML/CSS/JSON), stylua (Lua)
+-- JS/TS diagnostics handled by ts_ls (LSP) instead of eslint_d
+-- <leader>cf to format current buffer
+-- Prerequisites: `gem install erb_lint`, `:MasonInstall prettierd`
+-- See: https://github.com/nvimtools/none-ls.nvim
 return {
   "nvimtools/none-ls.nvim",
   dependencies = { "nvim-lua/plenary.nvim" },
@@ -8,12 +15,24 @@ return {
       sources = {
         null_ls.builtins.diagnostics.rubocop,
         null_ls.builtins.formatting.rubocop,
-        null_ls.builtins.diagnostics.eslint_d,
-        null_ls.builtins.formatting.eslint_d,
+        null_ls.builtins.formatting.prettierd.with({
+          filetypes = {
+            "javascript",
+            "javascriptreact",
+            "typescript",
+            "typescriptreact",
+            "html",
+            "css",
+            "json",
+            "yaml",
+            "markdown",
+          },
+        }),
+        null_ls.builtins.diagnostics.erb_lint,
         null_ls.builtins.formatting.stylua,
       },
     })
 
-    vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, { desc = "Format buffer" })
+    vim.keymap.set("n", "<leader>cf", vim.lsp.buf.format, { desc = "Format buffer" })
   end,
 }
