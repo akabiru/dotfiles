8a4de9034854f8860c95fe0000fd85d9c39fdd7d
Kabiru Mwenja <makabby@gmail.com>
2026-04-01 22:49:21 +0300

    Switch treesitter to auto_install for all languages

--- nvim/.config/nvim/lua/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -2,7 +2,7 @@ return {
   "nvim-treesitter/nvim-treesitter",
   build = ":TSUpdate",
   opts = {
-    ensure_installed = {"lua", "javascript", "typescript", "ruby"},
+    auto_install = true,
     highlight = { enable = true },
     indent = { enable = true },
   },
