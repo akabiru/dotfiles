10ba39d930e47441f1eabe9f8d9ff46efa27dea4
Kabiru Mwenja <makabby@gmail.com>
2026-04-01 22:50:27 +0300

    Add lualine with catppuccin-mocha theme

---
+++ nvim/.config/nvim/lua/plugins/lualine.lua
@@ -0,0 +1,19 @@
+return {
+  "nvim-lualine/lualine.nvim",
+  dependencies = { "nvim-tree/nvim-web-devicons" },
+  config = function()
+    require("lualine").setup({
+      options = {
+        theme = "catppuccin-mocha",
+      },
+      sections = {
+        lualine_a = { "mode" },
+        lualine_b = { "branch", "diff" },
+        lualine_c = { "filename" },
+        lualine_x = { "diagnostics" },
+        lualine_y = { "filetype" },
+        lualine_z = { "location" },
+      },
+    })
+  end,
+}
