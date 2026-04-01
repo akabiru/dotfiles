0422ea6e4768029500a9a9c71aa603104f9203aa
Kabiru Mwenja <makabby@gmail.com>
2026-04-01 23:21:54 +0300

    Enable catppuccin integrations for all plugins

--- nvim/.config/nvim/lua/plugins/catppuccin.lua
+++ nvim/.config/nvim/lua/plugins/catppuccin.lua
@@ -1,9 +1,31 @@
+-- Catppuccin: Soothing pastel color scheme for Neovim
+-- Applied globally via colorscheme, with integrations for most plugins
+-- See: https://github.com/catppuccin/nvim
 return {
   "catppuccin/nvim",
   name = "catppuccin",
   priority = 1000,
   config = function()
-    require("catppuccin").setup()
+    require("catppuccin").setup({
+      integrations = {
+        gitsigns = true,
+        mason = true,
+        neotree = true,
+        treesitter = true,
+        which_key = true,
+        bufferline = true,
+        mini = { enabled = true },
+        native_lsp = {
+          enabled = true,
+          underlines = {
+            errors = { "undercurl" },
+            hints = { "undercurl" },
+            warnings = { "undercurl" },
+            information = { "undercurl" },
+          },
+        },
+      },
+    })
     vim.cmd.colorscheme("catppuccin-mocha")
   end,
 }
