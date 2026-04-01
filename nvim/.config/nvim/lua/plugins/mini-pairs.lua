453f66f7119c802a1c22154796ada38cee58a15f
Kabiru Mwenja <makabby@gmail.com>
2026-04-01 23:23:47 +0300

    Add mini.pairs for automatic bracket and quote pairing

---
+++ nvim/.config/nvim/lua/plugins/mini-pairs.lua
@@ -0,0 +1,10 @@
+-- Mini.pairs: Automatic bracket/quote pairing
+-- Type ( and get (), type " and get "", etc.
+-- Works for: (), [], {}, "", '', ``
+-- See: https://github.com/echasnovski/mini.pairs
+return {
+  "echasnovski/mini.pairs",
+  version = "*",
+  event = "InsertEnter",
+  opts = {},
+}
