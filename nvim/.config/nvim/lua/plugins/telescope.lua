4d5f845fb0789b43b76efb51e43f7a4dd4cb4321
Kabiru Mwenja <makabby@gmail.com>
2026-04-01 21:56:40 +0300

    Add telescope-ui-select for Telescope-powered code action menu

--- nvim/.config/nvim/lua/plugins/telescope.lua
+++ nvim/.config/nvim/lua/plugins/telescope.lua
@@ -1,8 +1,20 @@
 return {
   "nvim-telescope/telescope.nvim",
   tag = "v0.2.1",
-  dependencies = { "nvim-lua/plenary.nvim" },
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+    { "nvim-telescope/telescope-ui-select.nvim" },
+  },
   config = function()
+    require("telescope").setup({
+      extensions = {
+        ["ui-select"] = {
+          require("telescope.themes").get_dropdown(),
+        },
+      },
+    })
+    require("telescope").load_extension("ui-select")
+
     local builtin = require("telescope.builtin")
     vim.keymap.set("n", "<C-p>", function() builtin.find_files({ hidden = true }) end, {})
     vim.keymap.set("n", "<leader>fg", function() builtin.live_grep({ additional_args = { "--hidden" } }) end, {})
