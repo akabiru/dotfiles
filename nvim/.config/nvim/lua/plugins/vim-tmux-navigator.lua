e8022c76421b75fd7350c28ab897090e2ff2e9e8
Kabiru Mwenja <makabby@gmail.com>
2026-04-02 18:17:19 +0300

    Fix vim-tmux-navigator keymaps not working on LazyVim dashboard

--- nvim/.config/nvim/lua/plugins/vim-tmux-navigator.lua
+++ nvim/.config/nvim/lua/plugins/vim-tmux-navigator.lua
@@ -4,4 +4,11 @@
 -- See: https://github.com/christoomey/vim-tmux-navigator
 return {
   "christoomey/vim-tmux-navigator",
+  lazy = false,
+  keys = {
+    { "<C-h>", "<cmd>TmuxNavigateLeft<cr>", desc = "Navigate left" },
+    { "<C-j>", "<cmd>TmuxNavigateDown<cr>", desc = "Navigate down" },
+    { "<C-k>", "<cmd>TmuxNavigateUp<cr>", desc = "Navigate up" },
+    { "<C-l>", "<cmd>TmuxNavigateRight<cr>", desc = "Navigate right" },
+  },
 }
