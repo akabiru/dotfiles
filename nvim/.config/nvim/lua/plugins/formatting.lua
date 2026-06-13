fd6cbfecc67937875a1e8728852bca175bf6ee08
Kabiru Mwenja <makabby@gmail.com>
2026-06-13 19:52:18 +0300

    Auto-install prettierd via Mason ensure_installed
    prettierd was referenced for markdown/JS/TS/web formatting but never
    installed, so <leader>cf silently no-op'd. Add it to mason.nvim's
    ensure_installed so it auto-installs on fresh machines.

--- nvim/.config/nvim/lua/plugins/formatting.lua
+++ nvim/.config/nvim/lua/plugins/formatting.lua
@@ -1,29 +1,37 @@
 -- Formatting: Code formatters via conform.nvim (LazyVim's default formatter)
 -- <leader>cf to format buffer (LazyVim default keymap)
 -- rubocop for Ruby, prettierd for JS/TS/web, stylua for Lua
--- Prerequisites: `:MasonInstall prettierd` or `npm i -g @fsouza/prettierd`
+-- prettierd is auto-installed via the mason.nvim spec below (ensure_installed).
 -- See: https://github.com/stevearc/conform.nvim
 return {
-  "stevearc/conform.nvim",
-  opts = {
-    formatters = {
-      rubocop = {
-        command = "bundle",
-        prepend_args = { "exec", "rubocop" },
+  {
+    "stevearc/conform.nvim",
+    opts = {
+      formatters = {
+        rubocop = {
+          command = "bundle",
+          prepend_args = { "exec", "rubocop" },
+        },
+      },
+      formatters_by_ft = {
+        ruby = { "rubocop" },
+        javascript = { "prettierd" },
+        javascriptreact = { "prettierd" },
+        typescript = { "prettierd" },
+        typescriptreact = { "prettierd" },
+        html = { "prettierd" },
+        css = { "prettierd" },
+        json = { "prettierd" },
+        yaml = { "prettierd" },
+        markdown = { "prettierd" },
+        lua = { "stylua" },
       },
     },
-    formatters_by_ft = {
-      ruby = { "rubocop" },
-      javascript = { "prettierd" },
-      javascriptreact = { "prettierd" },
-      typescript = { "prettierd" },
-      typescriptreact = { "prettierd" },
-      html = { "prettierd" },
-      css = { "prettierd" },
-      json = { "prettierd" },
-      yaml = { "prettierd" },
-      markdown = { "prettierd" },
-      lua = { "stylua" },
-    },
+  },
+  -- Auto-install formatters used above. LazyVim merges (not replaces) this
+  -- list, so prettierd is appended to its defaults (stylua, shfmt, etc.).
+  {
+    "mason-org/mason.nvim",
+    opts = { ensure_installed = { "prettierd" } },
   },
 }
