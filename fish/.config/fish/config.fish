9952be127af2923e180750a453761e00142e098e
Kabiru Mwenja <makabby@gmail.com>
2026-06-24 21:22:41 +0300

    Orbstack init

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -28,3 +28,7 @@ fish_add_path ~/.cargo/bin
 
 status --is-interactive; and nodenv init - fish | source
 starship init fish | source
+
+# Added by OrbStack: command-line tools and integration
+# This won't be added again if you remove it.
+source ~/.orbstack/shell/init2.fish 2>/dev/null || :
